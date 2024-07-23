b4634af92abb08e803dcff44852e89229f1609dc
Marie <mari.nazaryan7173@gmail.com>
2024-07-23 12:16:08 +0400

    Update commands list

--- sources/library.c
+++ sources/library.c
@@ -2,12 +2,17 @@
 
 static void	help(void)
 {
-	printf("%s--------------------------------------------------------%s\n", ORANGE, RESET);
-	printf("%s| Commands    | REGISTER    - Register new user        |%s\n", ORANGE, RESET);
-	printf("%s|             | LOGIN       - Log in to personal page  |%s\n", ORANGE, RESET);
-	printf("%s|             | ADD         - Add new book             |%s\n", ORANGE, RESET);
-	printf("%s|             | SEARCH      - Search a book            |%s\n", ORANGE, RESET);
-	printf("%s--------------------------------------------------------%s\n", ORANGE, RESET);
+	printf("%s---------------------------------------------------------%s\n", ORANGE, RESET);
+	printf("%s| Commands    | REGISTER   - Register new user          |%s\n", ORANGE, RESET);
+	printf("%s|             | LOGIN      - Log in to personal page    |%s\n", ORANGE, RESET);
+	printf("%s|             | LOGOUT     - Log out from personal page |%s\n", ORANGE, RESET);
+	printf("%s|             | ADD        - Add new book               |%s\n", ORANGE, RESET);
+	printf("%s|             | SEARCH     - Search a book              |%s\n", ORANGE, RESET);
+	printf("%s|             | BORROW     - Borrow a book              |%s\n", ORANGE, RESET);
+	printf("%s|             | RETURN     - Return a book              |%s\n", ORANGE, RESET);
+	printf("%s|             | RESERVE    - Reserve a book             |%s\n", ORANGE, RESET);
+	printf("%s|             | REMOVE     - Remove a book              |%s\n", ORANGE, RESET);
+	printf("%s---------------------------------------------------------%s\n", ORANGE, RESET);
 }
 
 int	main(int argc, char **argv)
